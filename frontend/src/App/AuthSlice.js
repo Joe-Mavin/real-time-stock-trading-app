e2b6d76e62a2f18c411526d1de2109e37a473e4d
Joe-mavin <mavinodundo@gmail.com>
2024-07-09 11:10:20 +0300

    Implement RegisterUser and LoginUser async thunks to handle user registration and login

--- frontend/src/App/AuthSlice.js
+++ frontend/src/App/AuthSlice.js
@@ -1,2 +1,24 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import authService from '../../services/api/authService';
+
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (userData, thunkAPI) => {
+    try {
+      return await authService.register(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (userData, thunkAPI) => {
+    try {
+      return await authService.login(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+)
